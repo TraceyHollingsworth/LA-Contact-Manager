d2a7fd8555c165208f1fa734780841b4ffbdab29
Tracey Hollingsworth <tracey.hollingsworth@gmail.com>
2014-12-04 19:36:40 -0500

    Active Record Practice

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+require_relative 'models/contact'
+also_reload 'models/contact'
+
+get '/' do
+  @contacts = Contact.all
+  erb :index
+end
